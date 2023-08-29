622981ab7993b08e6f4d9984e2e4c5eb26fab9f6
muhammadjoingithub <143167331+muhammadjoingithub@users.noreply.github.com>
2023-08-29 17:19:06 +0300

    Add files via upload
    Signed-off-by: muhammadjoingithub <143167331+muhammadjoingithub@users.noreply.github.com>

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-let hamMenuIcon = document.querySelectorAll(".ham-menu");
-let navBar = document.querySelectorAll("nav-bar");
+
+let hamMenuIcon = document.querySelectorAll(".fa-bars");
+let navBar = document.querySelectorAll(".items");
 let navLinks = document.querySelectorAll("li");
 
 hamMenuIcon.addEventListener("click" , () => {
     navBar.classList.toogle("active");
-    hamMenuIcon.classList.toogle("fa-times");
+    hamMenuIcon.classList.toogle(".fa-times");
 });
 
 
@@ -15,21 +15,16 @@ navLinks.forEach((navLinks) => {
         hamMenuIcon.classList.toogle("fa-times");
     });
 })
-=======
-let hamMenuIcon = document.querySelectorAll(".ham-menu");
-let navBar = document.querySelectorAll("nav-bar");
-let navLinks = document.querySelectorAll("li");
-
-hamMenuIcon.addEventListener("click" , () => {
-    navBar.classList.toogle("active");
-    hamMenuIcon.classList.toogle("fa-times");
-});
 
 
-navLinks.forEach((navLinks) => {
-    navLinks.addEventListener("click" , () => {
-        navBar.classList.toogle("active");
-        hamMenuIcon.classList.toogle("fa-times");
-    });
-})
->>>>>>> e69f11899f3fe6ddac207807e8e16b748bd093b6
+
+window.addEventListener('scroll', () => {
+    let header = document.querySelector('header');
+    let logoText = document.querySelector('.logoText');
+   
+    header.classList.toggle('sticky', window.scrollY > 0)
+    logoText.classList.toggle('logoTextNone', window.scrollY > 0)
+   })
+
+
+
